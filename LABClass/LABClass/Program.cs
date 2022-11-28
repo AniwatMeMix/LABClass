4b9da833429b260468b8b70c740ca16f7adf5574
Eggylnwza007 <kamom.j@kkumail.com>
2022-11-28 17:20:07 +0700

    add text

---
+++ LABClass/LABClass/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LABClass;
+using System;
+
+
+Student student1 = new Student();
+student1.name = "moragon";
+student1.weight = 67.3f;
+
+Student student2 = new Student();
+student1.name = "Mix";
+student1.weight = 66.5f;
+
+Student studen3 = new Student();
+student1.name = "flok";
+student1.weight = 83.2f;
+
+Student studen4 = new Student();
+student1.name = "kawpan";
+student1.weight = 140.5f;
+
+Student studen5 = new Student();
+student1.name = "figo";
+student1.weight = 67.5f;
+
+
+Console.WriteLine("น้ำหนักเฉลี่ยของทุกคน = 85");
+
+Console.WriteLine("น้ำหนักของคนที่มากที่สุด = kawpan นน.140.5");
+
+Console.WriteLine("น้ำหนักของคนที่น้อยที่สุด = Mix นน.66.5");
+
+Console.WriteLine("แสดงชื่อ รหัส ของผู้พัฒนาโปรแกรม = นาย อนิวัตติ์ ณ หนองคาย 653450106-7 ");
